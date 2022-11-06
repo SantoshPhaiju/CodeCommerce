6054d61a364756a63fffb06c4e50bb9ac75e219d
Santosh Phaiju <santoshphaiju062@gmail.com>
2022-11-06 17:17:36 +0545

    displaying tshirts in forntend from the backend

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,35 +1,37 @@
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 
 const ProductCard = (props) => {
+  const {title, desc, price, img, color, slug, size, availableQty, category} = props.details;
+  // console.log(props);
   return (
     <>
       <div className="xl:w-[24%] lg:w-[30%] md:w-[45%] pt-0 p-4 w-full border shadow-md rounded-md">
         <Link
-          href={`/products/tshirts`}
+          href={`/products/${slug}`}
           className="block relative rounded overflow-hidden"
         >
-          <Image
+          <img
             alt="ecommerce"
             className="object-top object-contain w-full h-[40vh] block mx-auto"
-            src={props.image}
+            src={img}
             width={300}
             height={400}
           />
         </Link>
         <div className="mt-4 text-center md:text-left">
           <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-            CATEGORY
+            {category}
           </h3>
           <h2 className="text-gray-900 title-font text-lg font-medium">
-            The Catalyzer
+            {title}
           </h2>
-          <p className="mt-1 font-robotoslab"> NRs.500.00</p>
+          <p className="mt-1 font-robotoslab"> NRs.{price}</p>
           <div className="footerContent block md:flex md:justify-between">
-            <p className="mt-1 font-firasans my-2">S, M, L, XL, XXL</p>
-            <Link href={"/products/tshirt"}>
+            <p className="mt-1 font-firasans my-2">{size}</p>
+            <Link href={`/products/${slug}`}>
               <button className="py-1 px-8 bg-green-700 hover:bg-green-800 hover:shadow-lg hover:shadow-gray-500 text-white font-firasans rounded-sm shadow-md shadow-gray-400 flex justify-center items-center space-x-1">
                 <HiOutlineShoppingCart className="text-lg -rotate-6" />
                 <span>Buy Now</span>
--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,7 +1,10 @@
+import mongoose from "mongoose";
 import React from "react";
 import ProductCard from "../components/ProductCard";
+import Product from "../models/Product";
 
-const Tshirts = () => {
+const Tshirts = ({products}) => {
+  // console.log(products);
   return (
     <div>
       <h2 className="text-center mt-10 font-firasans text-3xl -mb-10 text-purple-900/75 font-semibold">
@@ -10,28 +13,31 @@ const Tshirts = () => {
       <section className="text-gray-600 body-font">
         <div className="container py-24 sm:w-[50%] md:w-[90%] lg:w-[95%] w-[90%] mx-auto">
           <div className="flex flex-wrap justify-center gap-4">
-            <ProductCard
-              image="/tshirt.png"
-              title="I Fell in Love with an Nepali T-Shirt"
-              brand="Teeburon"
-            />
-            <ProductCard image="/tshirt2.jpg" />
-            <ProductCard image="/tshirt3.jpg" />
-            <ProductCard image="/tshirt4.jpg" />
-            <ProductCard image="/tshirt5.jpg" />
-            <ProductCard image="/tshirt6.jpg" />
-            <ProductCard image="/tshirt.png" />
-            <ProductCard image="/tshirt.png" />
-            <ProductCard image="/tshirt6.jpg" />
-            <ProductCard image="/tshirt.png" />
-            <ProductCard image="/tshirt5.jpg" />
-            <ProductCard image="/tshirt.png" />
-            <ProductCard image="/tshirt6.jpg" />
+            {
+              products.map((product) =>{
+                return (
+                <ProductCard
+                key={product._id}
+                  details={product}
+                /> );
+              })
+            }
           </div>
         </div>
       </section>
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  if(!mongoose.connections[0].readyState){
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+
+  let products = await Product.find({category: 'tshirt'});
+  return {
+    props: {products: JSON.parse(JSON.stringify(products))}, // will be passed to the page component as props
+  };
+}
 
 export default Tshirts;
