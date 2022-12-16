b8da0257a54af4f9944bc0dac2fd9c58cc45ec48
Santosh Phaiju <santoshphaiju062@gmail.com>
2022-12-16 15:00:29 +0545

    changes merged

--- pages/admin/components/Sidebar.js
+++ pages/admin/components/Sidebar.js
@@ -4,10 +4,6 @@ import React from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { RiEyeLine } from "react-icons/ri";
 import { RiUserLine } from "react-icons/ri";
-<<<<<<< HEAD
-import { MdDashboard } from 'react-icons/md'
-=======
->>>>>>> d11d37e5aac308441d2977ce769ed3b9dd8af6c0
 import { HiOutlineTemplate, HiOutlinePlusCircle } from "react-icons/hi";
 
 const Sidebar = ({ showSideBar, setShowSidebar, sideBarRef }) => {
