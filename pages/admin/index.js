6f748634da8dd5d9f7f201d524bea05b26719354
Santosh Phaiju <santoshphaiju062@gmail.com>
2022-12-16 15:07:16 +0545

    deleting material ui and charts

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,4 +1,3 @@
-import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
 import React, { useRef, useState } from "react";
 import AdminNav from "./components/AdminNav";
 import Sidebar from "./components/Sidebar";
@@ -32,29 +31,7 @@ const Index = () => {
       >
         <div className="maindiv text-black pt-0 mt-10">
           <h1 className="my-4 font-roboto text-2xl text-blue-700">DASHBOARD</h1>
-          <Card sx={{ background: "#ffaaff", minWidth: 275, width: 300, height: 200, boxShadow: "2px 2px 6px 2px rgba(0, 0, 0, 0.3)" }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="div">
-                
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-              <Typography variant="body2">
-                
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" variant="outlined">Learn More</Button>
-            </CardActions>
-          </Card>
+          
         </div>
       </div>
     </>
